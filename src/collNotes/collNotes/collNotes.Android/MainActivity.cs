fe283bda562b2608c84eabd89115c8030e179448
Jacob Motley <jacob.motley@protonmail.com>
2020-04-25 21:30:51 -0400

    added android splash screen

--- src/collNotes/collNotes/collNotes.Android/MainActivity.cs
+++ src/collNotes/collNotes/collNotes.Android/MainActivity.cs
@@ -7,7 +7,8 @@ using Plugin.Permissions;
 
 namespace collNotes.Droid
 {
-    [Activity(Label = "collNotes", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "collNotes", Icon = "@drawable/icon", Theme = "@style/MainTheme", 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ src/collNotes/collNotes/collNotes.Android/SplashScreenActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace collNotes.Droid
+{
+    [Activity(Label = "SplashScreenActivity", Theme = "@style/SplashTheme.Screen", MainLauncher = true, NoHistory = true)]
+    public class SplashScreenActivity : Activity
+    {
+        static readonly string TAG = "X:" + typeof(SplashScreenActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            Console.WriteLine(TAG, "SplashActivity.OnCreate");
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            Console.WriteLine(TAG, "starting MainActivity.");
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
